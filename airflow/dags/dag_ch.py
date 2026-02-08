49496ba6ab95292898766ecd23bc716eaa05351f
mvtyazhelkov <mtyazhelkov@yandex.ru>
2026-02-08 21:18:58 +0000

    add ClickhouseHook

--- airflow/dags/dag_ch.py
+++ airflow/dags/dag_ch.py
@@ -4,7 +4,11 @@ from datetime import datetime
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow_clickhouse_plugin.operators.clickhouse import ClickHouseOperator
 from airflow_clickhouse_plugin.hooks.clickhouse import ClickHouseHook
-    
+
+def insert_to_clickhouse(csv_file, table_name):
+    ch_hook = ClickHouseHook(clickhouse_conn_id='clickhouse_default')
+    ch_hook.execute('''INSERT INTO test (id, name) VALUES(2,'Anton');''')   
+
 dag=DAG(
     'test',
     schedule_interval='@daily',
@@ -14,8 +18,7 @@ dag=DAG(
 
 insert_data=ClickHouseOperator(
     task_id='create_table',
-    sql='''INSERT INTO test (id, name) VALUES(2,'Anton');''',
-    clickhouse_conn_id='clickhouse_default',
+    python_callable=insert_to_clickhouse,
     dag=dag,
     )
 
