35c2ddda40bfe7bdddb9b34b95fc230860a5c7d5
mvtyazhelkov <mtyazhelkov@yandex.ru>
2026-02-11 20:03:53 +0000

    check names

--- scripts/test_sript.py
+++ scripts/test_sript.py
@@ -3,7 +3,8 @@ from airflow_clickhouse_plugin.hooks.clickhouse import ClickHouseHook
 
 def insert_into_ch_new():
     #Здесь может быть питон код
+
     print(f"Доступные имена: {globals().keys()}")
     #sql
-    ch_hook=ClickHouseHook(clickhouse_conn_id='clickhouse_default')
-    ch_hook.execute('''INSERT INTO test(id,name) VALUES(3, 'Kristina')''')
+    #ch_hook=ClickHouseHook(clickhouse_conn_id='clickhouse_default')
+    #ch_hook.execute('''INSERT INTO test(id,name) VALUES(3, 'Kristina')''')
