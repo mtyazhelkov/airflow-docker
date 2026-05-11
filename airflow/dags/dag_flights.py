1405aca711c236c79f7725b208029e90248e2e43
mvtyazhelkov <mtyazhelkov@yandex.ru>
2026-05-11 12:07:36 +0000

    flight dags

--- airflow/dags/dag_flights.py
+++ airflow/dags/dag_flights.py
@@ -11,15 +11,17 @@ import os
 sys.path.insert(0, '/project')
 
 from scripts.departure_flights import departure_flights
+from scripts.arrivals_flights import arrivals_flights
 
 with DAG(
    'flights',
-    schedule_interval='@daily',
+    schedule_interval='5 07 * * *',
     start_date=days_ago(1),
     #end_date=datetime(2027,1,1),
     max_active_runs=1
 ) as dag:
   
   task1=departure_flights()
+  task2=arrivals_flights()
 
-task1
+task1>>task2
---
+++ scripts/arrivals_flights.py
@@ -0,0 +1,75 @@
+from airflow.decorators import task
+from scripts.class_download import DownloadFlightsData
+from scripts.class_token import TokenManager
+from datetime import datetime, timedelta, time
+
+import sys
+import os
+
+# Явно добавляем путь к папке, где лежит папка scripts
+sys.path.insert(0, '/project')
+
+@task
+def arrivals_flights():
+    
+     # 1. Получаем дату вчерашнего дня
+     yesterday_dt = datetime.now().date() - timedelta(days=1)
+
+     # 2. Получаем  первую и последнюю дату вчерашнего дня
+     fst_ts=int(datetime.combine(yesterday_dt, time.min).timestamp())
+     lst_ts=int(datetime.combine(yesterday_dt, time.max).timestamp())
+   
+     #Загрузка данных за вчерашний день
+     URL= f"https://opensky-network.org/api/flights/arrival?airport=EDDF&begin={fst_ts}&end={lst_ts}"
+     query='''
+             -- Используем json.dumps для превращения списка в одну JSON-строку
+             -- jsonb_populate_recordset автоматически разложит ключи по колонкам таблицы flights.tb_arrivals
+             insert into flights.tb_arrivals (
+                  icao24,
+                  firstseen,
+                  est_departure_airport,
+                  lastseen,
+                  est_arrival_airport,
+                  callsign,
+                  est_departure_airport_horiz_distance,
+                  est_departure_airport_vert_distance,
+                  est_arrival_airport_horiz_distance,
+                  est_arrival_airport_vert_distance,
+                  departure_airport_candidates_count,
+                  arrival_airport_candidates_count         
+             )
+             select 
+             "icao24" ,
+             "firstSeen",
+             "estDepartureAirport",
+             "lastSeen",
+             "estArrivalAirport",
+             "callsign",
+             "estDepartureAirportHorizDistance",
+             "estDepartureAirportVertDistance",
+             "estArrivalAirportHorizDistance",
+             "estArrivalAirportVertDistance",
+             "departureAirportCandidatesCount",
+             "arrivalAirportCandidatesCount"   
+             from jsonb_to_recordset(%s)
+             AS x(
+                  "icao24" text,
+                  "firstSeen" INT,
+                  "estDepartureAirport" text,
+                  "lastSeen" INT,
+                  "estArrivalAirport" text,
+                  "callsign" text,
+                  "estDepartureAirportHorizDistance" INT,
+                  "estDepartureAirportVertDistance" INT,
+                  "estArrivalAirportHorizDistance" INT,
+                  "estArrivalAirportVertDistance" INT,
+                  "departureAirportCandidatesCount" INT,
+                  "arrivalAirportCandidatesCount" INT     
+                 )
+                 ON CONFLICT (icao24) DO NOTHING;
+             ;   
+               '''
+     #Создаем экземпляр класса DownloadFlightsData
+     data=DownloadFlightsData(URL, query,1000)
+     #Вызываем метод download_data для загрузки данных
+     data.download_data()
